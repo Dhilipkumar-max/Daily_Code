5186b985d34c74614cbd9c2c5d2d263951323f7d
Dhilip Kumar <rajdhilipkumar20@gmail.com>
2025-10-20 20:41:48 +0530

    Create Length_of_String.java

---
+++ Length_of_String.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    String str = sc.next();
+    int length = str.length();
+    System.out.println("Length of String is "+length);
+  }
+}
