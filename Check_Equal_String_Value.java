747e4e7fd39e1866677690fa46ecad9c15d79d53
Dhilip Kumar <rajdhilipkumar20@gmail.com>
2025-10-23 21:09:50 +0530

    Create Check_Equal_String_Value.java

---
+++ Check_Equal_String_Value.java
@@ -0,0 +1,11 @@
+public class Main{
+  public static void main(String[] args){
+    String str1="hi";
+    String str2="hi";
+    if(str1.equals(str2)){
+      System.out.println("Equal");
+    }else{
+      System.out.println("Not Equal");
+    }
+  }
+}
