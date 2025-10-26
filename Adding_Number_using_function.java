f13ad0fcb8935451f72570d1200899910257b09d
Dhilip Kumar <rajdhilipkumar20@gmail.com>
2025-10-26 23:44:43 +0530

    Create Adding_Number_using_function.java

---
+++ Adding_Number_using_function.java
@@ -0,0 +1,12 @@
+public class Main{
+  public void add(int a, int b){
+    System.out.println(a+b);
+  }
+  public static void main(String[] args){
+    Main n = new Main();
+    int a =2;
+    int b= 2;
+    n.add(a,b);
+  }
+}
+    
