0a948f482b9ba99364015ecdb0f6e5d25b70b4da
Dhilip Kumar <rajdhilipkumar20@gmail.com>
2026-03-19 19:59:16 +0530

    Update ConcatenatedBinary.java

--- ConcatenatedBinary.java
+++ ConcatenatedBinary.java
@@ -5,7 +5,6 @@ class Solution {
         int bitLength = 0;
 
         for (int i = 1; i <= n; i++) {
-            // Check if i is a power of 2 to increment bitLength
             // e.g., 1 (1), 2 (10), 4 (100), 8 (1000)...
             if ((i & (i - 1)) == 0) {
                 bitLength++;
