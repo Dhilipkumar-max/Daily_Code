f7ef81a044b6529831d4f3d3067b993bb3a7a380
Dhilip Kumar <rajdhilipkumar20@gmail.com>
2026-04-18 20:20:47 +0500

    Update CheckStrings.py

--- CheckStrings.py
+++ CheckStrings.py
@@ -1,10 +1,9 @@
 class Solution(object):
     def checkStrings(self, s1, s2):
-        # Compare characters at even indices
+       
         even_s1 = sorted(s1[0::2])
         even_s2 = sorted(s2[0::2])
         
-        # Compare characters at odd indices
         odd_s1 = sorted(s1[1::2])
         odd_s2 = sorted(s2[1::2])
         
