e42577abaf99e2242bfb27f4a84c6b3dcb548fd0
Dhilip Kumar <rajdhilipkumar20@gmail.com>
2026-05-07 20:50:36 +0530

    Update DSU.py

--- DSU.py
+++ DSU.py
@@ -19,12 +19,6 @@ class DSU:
 
 class Solution(object):
     def minimumHammingDistance(self, source, target, allowedSwaps):
-        """
-        :type source: List[int]
-        :type target: List[int]
-        :type allowedSwaps: List[List[int]]
-        :rtype: int
-        """
         n = len(source)
         dsu = DSU(n)
         
