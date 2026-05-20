c48bf09d3171cc36fe88566b962db920e21c164c
Dhilip Kumar <rajdhilipkumar20@gmail.com>
2026-05-20 22:31:45 +0530

    Create FindThe_Prefix_Common_Array.py

---
+++ FindThe_Prefix_Common_Array.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findThePrefixCommonArray(self, A, B):
+        n = len(A)
+        freq = [0] * (n + 1)
+        C = []
+        common_count = 0
+        
+        for i in range(n):
+            # Process element from A
+            freq[A[i]] += 1
+            if freq[A[i]] == 2:
+                common_count += 1
+                
+            # Process element from B
+            freq[B[i]] += 1
+            if freq[B[i]] == 2:
+                common_count += 1
+                
+            # Append the count of common elements at this prefix
+            C.append(common_count)
+            
+        return C
+        
